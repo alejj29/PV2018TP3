cab4329d8ab20f1c87decb87e4da69a89a268bbd
pc <pc@DESKTOP-KL2HFPD>
2018-04-14 15:35:48 -0300

    autor: Juan José Alancay grupo 15 Punto 4 resuelto

---
+++ src/Punto4/principal.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Punto4;
+
+/**
+ *
+ * @author Juan José Alancay grupo 15
+ */
+public class principal {
+    public static void main(String[] args) {
+        programa mostrar = new programa ();
+        mostrar.compararSumaPromedio();
+    }
+}
