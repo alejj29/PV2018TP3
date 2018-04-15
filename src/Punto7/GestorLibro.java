1d3ffb5f5b7185dcdd1ed3f47958417658b4032b
ale <ale@ale-PC>
2018-04-15 16:48:25 -0300

    Realise  el Punto 7.Usuario alejandro

---
+++ src/Punto7/GestorLibro.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Punto7;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author ale
+ */
+public class GestorLibro {
+    ArrayList<Libro> libros;
+    
+    public GestorLibro() {
+        libros=new ArrayList<Libro>();
+    }
+    public void reporte(){
+        System.out.println("\nISBM\tAutor\tTitulo\tPrecio");
+        for (Libro  l: libros) {
+            System.out.println(l);
+        }
+        
+    }
+   public void registrar(Libro l){
+       libros.add(l);
+       reporte();
+       System.out.println("-----------------------------------");
+   }
+}
