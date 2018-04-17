052e0cad27da7ad8f8f36718fabfb7d41d190c8f
pc <pc@DESKTOP-KL2HFPD>
2018-04-17 12:13:57 -0300

    juan josé Alancay punto12 resuelto con un pequeño error que sale en la opcion 2 el error no afecta al resultado de la opciones si lo pueden encontrar al error seria bueno

---
+++ src/Punto12/Categoria.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Punto12;
+
+/**
+ *
+ * @author Alancay Juan José
+ */
+public class Categoria {
+    private String pro;
+
+    public Categoria(String pro) {
+        this.pro = pro;
+    }
+
+    public String getPro() {
+        return pro;
+    }
+
+    public void setPro(String pro) {
+        this.pro = pro;
+    }
+
+   
+}
