818ce80ca9b22fe3eb852ac9079851774c9f00a9
pc <pc@DESKTOP-KL2HFPD>
2018-04-17 13:15:20 -0300

    juan josé Alancay problema resuelto del punto 12 tema de bluckes que me generaba con while un error

--- src/Punto12/MenuPrincipal.java
+++ src/Punto12/MenuPrincipal.java
@@ -11,6 +11,12 @@ import java.util.Scanner;
 /**
  * @author Juan Jose Alancay
  */
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+/**
+ * @author Juan Jose Alancay
+ */
 public class MenuPrincipal {
     public static void main(String[] args) {
       Scanner sc = new Scanner (System.in);
@@ -25,64 +31,74 @@ public class MenuPrincipal {
       cat.add(new Categoria ("Literatura"));
       cat.add(new Categoria ("Historia"));
 
-      //creando menu de opciones
-      int opcion;
+     
+      int opcion=0;
+      int salir=0;
+      while(opcion!=4){//bucle de salida
+           //menu de opciones
       System.out.println("---------Bienvenido Biblioteca UNJU-----------");
         System.out.println("Elija la forma de busqueda:");
         System.out.println("1: busqueda por autor");
         System.out.println("2: busqueda por categoria");
         System.out.println("3: busqueda por ambas cosas");
-        System.out.println("");
+        System.out.println("4: salir");
        opcion=sc.nextInt(); 
-      boolean bandera=true;
+      int bandera=0;
       int i=0;
       String catego;
-      //opcion elejida del menu
        switch (opcion){
          case 1:System.out.println("ingrese el autor");
-          String nombre=sc.next();   
-          while(bandera==true){//bucle de stop de la busqueda
+          String nombre=sc.next();  
+             System.out.println("");
+          for(i=0; i<buscar.size();i++){//bucle de stop de la busqueda
               if(nombre.equals(buscar.get(i).getAut())){// bucle de concidencia
-                  System.out.println("libro encontrado");
+                  System.out.println("libro encontrado:");
                   System.out.println("ISBN: "+buscar.get(i).getIs()+" | "+"AUTOR: "+buscar.get(i).getAut()+
                           " | Categoria: "+cat.get(i).getPro());
-                  bandera=false;
+                   bandera=1;
               }
-              i++;
           }
+             System.out.println("");
           break;        
         case 2:
             System.out.println("ingrese una categoria");
             catego=sc.next();
-            while (bandera=true){//bucle de stop
-                if(catego.equals(cat.get(i).getPro())){//bucle de concidencias
+            System.out.println("");
+            for (i=0;i<cat.size();i++){//bucle de busqueda
+                if(catego.equals(cat.get(i).getPro())){//setenia de concidencia
                     System.out.println("libro encontrado:");//visualizacion del libro
                    System.out.println("ISBN: "+buscar.get(i).getIs()+" | "+"AUTOR: "+buscar.get(i).getAut()+
                           " | Categoria: "+cat.get(i).getPro());
-                  bandera=false;
-                }
-              i++;   
+                  bandera=1;
+                }  
             }
-            
+            System.out.println("");
            break; 
         case 3:
             System.out.println("ingrese autor");//ingreso de autor
             nombre=sc.next();
             System.out.println("ingrese categoria");// ingreso de categoria
             catego=sc.next();
-            while (bandera=true){// bucle de stop
-              if ((nombre.equals(buscar.get(i).getAut()))&&(catego.equals(cat.get(i).getPro()))){
-                  System.out.println("lbro encontrado:");//visualizacion
-                  System.out.println("ISBN: "+buscar.get(i).getIs()+" | "+"AUTOR: "+buscar.get(i).getAut()+
-                          " | Categoria: "+cat.get(i).getPro());
-                  bandera=false;    
-              }
-              i++;
+            System.out.println("");
+            for (i=0; i<buscar.size();i++){
+                  if (nombre.equals(buscar.get(i).getAut())){//sentencia de concidencia
+                     for(int j=0; j<cat.size();j++){//bucle busqueda
+                           if(catego.equals(cat.get(j).getPro())){// bucle de busqueda
+                              System.out.println("lbro encontrado:");//visualizacion
+                              System.out.println("ISBN: "+buscar.get(i).getIs()+" | "+"AUTOR: "+buscar.get(i).getAut()+
+                                " | Categoria: "+cat.get(i).getPro());
+                              bandera=1; 
+                           }
+                     }
+                  }
             }
+            System.out.println("");
              break;
-        default : System.out.println("opcion mal ingresada");
-            break;
       }
-    }
+      if(bandera==0 && (opcion!=4)){//sentencia de libro no encontra con bandera
+      System.out.println("libro no encontrado");
+      }
+      }
+   }
     
 }
